0f18f256f880af98ee14da7f1049c2b20f056217
sai pavani nagisetti <pavaninagisetti@sais-MBP.lan>
2021-03-28 02:15:13 -0700

    added dispatch to show cart items count

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import "./styles.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import MenuItemSelectionForm from '../src/MenuItemSelectionForm.js'
+import menuItems from "./data.json";
+
+const initialState = {
+  itemsInCart: []
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "handleCartItems":
+      return {
+        ...state,
+        itemsInCart: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
 
-  const [itemsAddedToCart, setItems] = useState();
-
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('items'));
-    setItems(items);
-  }, []);
+  const [itemsAddedToCart, setItems] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  function handleClick(e) {
+    const itemIndex = e.target.id;
+    itemsAddedToCart.push(menuItems.menu[itemIndex]);
+    setItems(itemsAddedToCart);
+    dispatch({ type: "handleCartItems", payload: itemsAddedToCart});
+  }
 
   return (
     <div className="App">
       <div className="header">
-        <button className="cart-button"><i className="fa fa-shopping-cart"></i> {itemsAddedToCart && itemsAddedToCart.length } | items </button>
+        <button className="cart-button"><i className="fa fa-shopping-cart"></i> { state.itemsInCart && state.itemsInCart.length } | items </button>
       </div>
-      <MenuItemSelectionForm />
+      <MenuItemSelectionForm handleClick={ handleClick } />
     </div>
   );
 }
--- src/MenuItemSelectionForm.js
+++ src/MenuItemSelectionForm.js
@@ -2,14 +2,8 @@ import "./styles.css";
 import React, { useState } from "react";
 import menuItems from "./data.json";
 
-function MenuItemSelectionForm() {
-  const [itemsAddedToCart, setItemToCart] =  useState(JSON.parse(localStorage.getItem('items')) || []);
-  function handleClick(e) {
-    const itemIndex = e.target.id;
-    itemsAddedToCart.push(menuItems.menu[itemIndex]);
-    setItemToCart(itemsAddedToCart);
-    localStorage.setItem('items', JSON.stringify(itemsAddedToCart));
-  }
+function MenuItemSelectionForm(props) {
+
   return (
     <div className="menu-item-selection-form">
         {menuItems.menu &&
@@ -17,7 +11,7 @@ function MenuItemSelectionForm() {
             <div className="menu-items" key={ index }>
               <div> { item.name } </div>
               <div className="menu-item-price"> ${ item.price }</div>
-              <button className="add-to-cart-button" onClick={ handleClick } id={ index }> Add to Cart </button>
+              <button className="add-to-cart-button" onClick={ props.handleClick } id={ index }> Add to Cart </button>
             </div>
           ))}
       </div>
