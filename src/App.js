ac6e788bc17d85443c89f352f572ce6d13190c83
sai pavani nagisetti <pavaninagisetti@sais-MBP.lan>
2021-03-29 17:36:57 -0700

    showing order items

--- src/App.js
+++ src/App.js
@@ -31,10 +31,37 @@ function App() {
     dispatch({ type: "handleCartItems", payload: itemsAddedToCart});
   }
 
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "275px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
   return (
     <div className="App">
       <div className="header">
-        <button className="cart-button"><i className="fa fa-shopping-cart"></i> { state.itemsInCart && state.itemsInCart.length } | items </button>
+        <button className="cart-button" onClick={ openNav }><i className="fa fa-shopping-cart"></i> { state.itemsInCart && state.itemsInCart.length } | items </button>
+      </div>
+      <div id="mySidenav" className="sidenav">
+        <div className="sidenav-content">
+          <h2 className="order-header"> Your Order</h2>
+          <a className="closebtn" onClick={ closeNav }>&times;</a>
+          { state.itemsInCart.length > 0 ? 
+          state.itemsInCart.map((item, index) =>(
+            <div className="items-container">
+              <p>{ index+1 } { item.name } </p>
+              <p>{ item.price }</p>
+            </div>
+        )) 
+        :
+        <div className="empty-cart">
+          <p><i className="fa fa-shopping-cart"></i></p>
+          <h2>Your cart is empty</h2>
+          <button className="start-order-button" onClick={ closeNav }> Start your order</button>
+        </div> }
+        </div>
       </div>
       <MenuItemSelectionForm handleClick={ handleClick } />
     </div>
